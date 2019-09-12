46b69f13b3e50739b312006ccc06b1be6a041e4e
Joe Burmeister <joe.burmeister@devtank.co.uk>
2019-09-12 12:43:15 +0100

    Fix stupid PWM code mistake.

--- pwm.c
+++ pwm.c
@@ -5,6 +5,7 @@
 
 #include "config.h"
 #include "pinmap.h"
+#include "log.h"
 #include "pwm.h"
 
 static unsigned pwm_freq = 1000;
@@ -15,26 +16,26 @@ void pwm_init()
 {
     rcc_periph_clock_enable(RCC_TIM1);
 
-    timer_disable_counter(RCC_TIM1);
+    timer_disable_counter(TIM1);
 
-    timer_set_mode(RCC_TIM1,
+    timer_set_mode(TIM1,
         TIM_CR1_CKD_CK_INT,
         TIM_CR1_CMS_EDGE,
         TIM_CR1_DIR_UP);
-    timer_set_prescaler(RCC_TIM1, 48);
+    timer_set_prescaler(TIM1, 48);
 
-    timer_enable_preload(RCC_TIM1);
-    timer_continuous_mode(RCC_TIM1);
+    timer_enable_preload(TIM1);
+    timer_continuous_mode(TIM1);
 
-    timer_set_period(RCC_TIM1,48000000/48);
+    timer_set_period(TIM1,48000000/48);
 
-    timer_disable_oc_output(RCC_TIM1, TIM_OC1);
-    timer_set_oc_mode(RCC_TIM1, TIM_OC1, TIM_OCM_PWM1);
-    timer_enable_oc_output(RCC_TIM1, TIM_OC1);
+    timer_disable_oc_output(TIM1, TIM_OC1);
+    timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM1);
+    timer_enable_oc_output(TIM1, TIM_OC1);
 
-    timer_enable_counter(RCC_TIM1);
+    timer_enable_counter(TIM1);
 
-    timer_set_oc_value(RCC_TIM1, TIM_OC1, 0);
+    timer_set_oc_value(TIM1, TIM_OC1, 0);
     
     const port_n_pins_t port_n_pins[] = PWM_PORT_N_PINS;
 
@@ -53,13 +54,13 @@ void pwm_init()
 
 void pwm_set(unsigned freq, unsigned duty)
 {
-    timer_set_period(TIM14, 48000000/48/freq);
+    timer_set_period(TIM1, 48000000/48/freq);
 
     duty = 100 - duty;
 
-    timer_set_oc_value(TIM14, TIM_OC1, 48000000/48/freq * duty / 2 / 100);
+    timer_set_oc_value(TIM1, TIM_OC1, 48000000/48/freq * duty / 2 / 100);
 
-    timer_set_counter(TIM14, 0);
+    timer_set_counter(TIM1, 0);
 
     pwm_freq = freq;
     pwm_duty = duty;
